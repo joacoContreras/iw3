f4f57feb98696b97f6db803937cd7d9b8f717068
joacoContreras <cjoaquin835@gmail.com>
2026-08-27 12:39:26 +0000

    feature: more exceptions and interface-product-business

--- src/main/java/ar/edu/iua/iw3/business/exception/FoundException.java
+++ src/main/java/ar/edu/iua/iw3/business/exception/FoundException.java
@@ -1,5 +1,24 @@
 package ar.edu.iua.iw3.business.exception;
 
-public class FoundException {
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class FoundException extends Exception {
+
+    @Builder
+    public FoundException(String message, Throwable ex) {
+        super(message, ex);
+    }
+
+    @Builder
+    public FoundException(String message) {
+        super(message);
+    }
+
+    @Builder
+    public FoundException(Throwable ex) {
+        super(ex);
+    }
 
 }
